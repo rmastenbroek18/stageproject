8f621ddb220a1ad9dd75059adbae41d4b08d6e71
Rick kraster <Rick kraster>
2017-04-20 14:41:21 +0200

    hhh

--- node/src/app/app.component.ts
+++ node/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-roottt',
   template: '<kandidaat-form></kandidaat-form>',
   styleUrls: ['./app.component.css']
 })
